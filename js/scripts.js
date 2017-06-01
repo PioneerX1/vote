5cbe76e728a4e8a6726d270a4f55bc570c883e35
Alena Golovina and Mick Sexton <alena@>
2017-06-01 14:30:45 -0700

    add fix for js form to hide the opposite choice when other is invoked

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  
+  $("#age-form").submit(function() {
+    //alert("js function called");
+
+
+    var inputAge = $("input#age").val();
+    var inputAge = parseInt(inputAge);
+
+    if (inputAge >= 18) {
+      $('#old-enough').show();
+      $("#too-young").hide();
+    } else {
+      $('#too-young').show();
+      $("#old-enough").hide();
+    }
+
+
+
+
+    event.preventDefault();
+
+  });
 });
